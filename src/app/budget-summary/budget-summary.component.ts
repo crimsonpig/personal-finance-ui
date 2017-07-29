a4f2081a3fba078fde8a940096672017d92c33f8
crimsonpig <keith.stabbert@gmail.com>
2017-07-28 23:56:10 -0700

    Building out budget-summary component wireframe

--- src/app/budget-summary/budget-summary.component.ts
+++ src/app/budget-summary/budget-summary.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategorizedAmount } from '../categorizedamount';
+
 @Component({
   selector: 'app-budget-summary',
   templateUrl: './budget-summary.component.html',
   styleUrls: ['./budget-summary.component.css']
 })
 export class BudgetSummaryComponent implements OnInit {
+
+  incomes: CategorizedAmount[] = [
+    { category: 'PAYCHECK', amount: 5000.22 },
+    { category: 'CREDIT CARD REWARDS', amount: 125 }
+  ];
+
+  expenses: CategorizedAmount[] = [
+    { category: 'HOUSEHOLD', amount: 75 },
+    { category: 'GAS', amount: 150 },
+    { category: 'GROCERIES', amount: 400 },
+  ];
 
   constructor() { }
 
