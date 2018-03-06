cf90b4dda500da72a643860873f1109912b89be0
crimsonpig <keith.stabbert@gmail.com>
2018-03-05 22:37:05 -0800

    Correcting component ID

--- src/app/transaction-summary/transaction-summary.component.ts
+++ src/app/transaction-summary/transaction-summary.component.ts
@@ -29,7 +29,7 @@ export class TransactionSummaryComponent implements OnInit {
 
   @ViewChild(MatSort) sort: MatSort;
 
-  @ViewChild(CategorizedAmountsComponent) categorizedAmountsComponent: CategorizedAmountsComponent;
+  @ViewChild("incomeSummary") categorizedAmountsComponent: CategorizedAmountsComponent;
 
   @ViewChild("incomeSort") incomeSort: MatSort;
   @ViewChild("expensesSort") expensesSort: MatSort;
